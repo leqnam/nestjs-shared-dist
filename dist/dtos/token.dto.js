7d5713d45d2f370f0ab48a463987d5161c05158b
Nam Le <leqnam@live.com>
2025-11-04 12:22:06 +0700

    update

--- dist/dtos/token.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TokenDto = void 0;
-const abstract_dto_1 = require("./abstract.dto");
-class TokenDto extends abstract_dto_1.AbstractDto {
-}
-exports.TokenDto = TokenDto;
